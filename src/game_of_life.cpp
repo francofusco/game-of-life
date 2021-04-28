73737437e466f082a6ae693dd829a2202c861760
Franco Fusco <francofusco93@gmail.com>
2021-04-28 10:31:52 +0200

    Age and colors

--- src/game_of_life.cpp
+++ src/game_of_life.cpp
@@ -3,6 +3,9 @@
 #include <piksel/ext/rng.hpp>
 
 
+const glm::vec4 GameOfLife::CELL_COLOR = glm::vec4(0.2,1.0,0.2,1);
+
+
 GameOfLife::GameOfLife(
   unsigned int width,
   unsigned int height
@@ -24,12 +27,33 @@ void GameOfLife::setup()
   max_update_counter_ = 5;
   // initialize the grid
   grid_ = std::vector<int>(ROWS*COLS, 0);
-  auto r=ROWS/2, c=COLS/2;
-  toggle(r-1,c+1);
+  auto r = ROWS/2 + 3;
+  auto c = COLS/2;
+
+  // glider
+  // toggle(r-1,c+1);
+  // toggle(r  ,c+1);
+  // toggle(r+1,c+1);
+  // toggle(r+1,c  );
+  // toggle(r  ,c-1);
+
+  // tumbler
   toggle(r  ,c+1);
-  toggle(r+1,c+1);
-  toggle(r+1,c  );
+  toggle(r  ,c+2);
+  toggle(r+1,c+2);
+  toggle(r+2,c+1);
+  toggle(r+3,c+2);
+  toggle(r+4,c+3);
+  toggle(r+3,c+4);
+  toggle(r+2,c+4);
   toggle(r  ,c-1);
+  toggle(r  ,c-2);
+  toggle(r+1,c-2);
+  toggle(r+2,c-1);
+  toggle(r+3,c-2);
+  toggle(r+4,c-3);
+  toggle(r+3,c-4);
+  toggle(r+2,c-4);
 }
 
 
@@ -57,6 +81,22 @@ int GameOfLife::modulo(
 }
 
 
+void GameOfLife::setColor(
+  int age,
+  piksel::Graphics& g
+)
+{
+  // force age to be in the interval [1,MAX_AGE]
+  age = std::max(1, std::min(MAX_AGE, age));
+  // interpolation factor such that rho(1)=0.0 and rho(MAX_AGE)=1
+  auto rho = (age-1.0)/(MAX_AGE-1.0);
+  auto color = static_cast<float>(1-0.4*rho) * CELL_COLOR;
+  // apply the color
+  g.fill(color);
+  g.stroke(color);
+}
+
+
 void GameOfLife::draw(
   piksel::Graphics& g
 )
@@ -71,28 +111,28 @@ void GameOfLife::draw(
     g.fill(glm::vec4(textcolor,textcolor,textcolor,1));
     g.stroke(glm::vec4(textstroke,textstroke,textstroke,1));
     g.textSize(30.);
-    g.text("Game of Life", 110, 50);
+    g.text("Game of Life", 120, 50);
     g.textSize(18.);
     g.text(
       "Mouse click :  toggle cell\n"\
       "   space :  play/pause\n"\
       "   +/- :  change speed\n"\
+      "      C :  clear\n"\
       "      R :  randomize",
-      80, 100
+      90, 100
     );
 
   }
 
   // Apply some scaling to make it easier to see the points
   g.scale(PIXEL_SIZE, PIXEL_SIZE);
-  // pixels will be drawn in white
-  g.fill(glm::vec4(1,1,1,1));
-  g.stroke(glm::vec4(1,1,1,1));
 
   // print the current grid state
   for(unsigned int row=0; row<ROWS; row++) {
     for(unsigned int col=0; col<COLS; col++) {
       if(cell(row,col)) {
+        // set color by age
+        setColor(cell(row,col), g);
         // NOTE: the point is centered in the given coordinates, so we need to
         // shift them by half a pixel
         g.point(0.5+col,0.5+row);
@@ -124,6 +164,8 @@ void GameOfLife::updateGrid()
 {
   // this list will contain the index of every cell that has to be changed
   std::vector<int> to_change;
+  // while this keeps the list of cells whose age should be increased!
+  std::vector<int> to_age;
   // check each grid location to see if we should update it
   for(unsigned int row=0; row<ROWS; row++) {
     for(unsigned int col=0; col<COLS; col++) {
@@ -132,6 +174,8 @@ void GameOfLife::updateGrid()
         // a cell with less than 2 or more than 3 neighbours will die
         if(neighbours < 2 || neighbours > 3)
           to_change.push_back(gridIndex(row,col));
+        else
+          to_age.push_back(gridIndex(row, col));
       }
       else {
         // an empty spot with 3 neighbours will populate
@@ -143,6 +187,9 @@ void GameOfLife::updateGrid()
   // apply all needed changes
   for(const auto& i : to_change) {
     toggle(i);
+  }
+  for(const auto& i : to_age) {
+    grid_.at(i)++;
   }
 }
 
@@ -169,6 +216,15 @@ int GameOfLife::aliveNeighbours(int row, int col) {
     alives++;
   // return the final value
   return alives;
+}
+
+
+void GameOfLife::toggle(int i) {
+  auto& c = grid_.at(i);
+  if(c > 0)
+    c = 0;
+  else
+    c = 1;
 }
 
 
@@ -202,6 +258,13 @@ void GameOfLife::keyPressed(
   if(key == 333) {
     max_update_counter_++;
     DEBUG("max_update_counter_ incremented to " << max_update_counter_);
+  }
+
+  if(key == 'C') {
+    DEBUG("Removing all cells");
+    for(auto& c : grid_) {
+      c = 0;
+    }
   }
 
   if(key == 'R') {
--- src/game_of_life.hpp
+++ src/game_of_life.hpp
@@ -5,8 +5,10 @@
 
 class GameOfLife : public piksel::BaseApp {
 public:
-  static unsigned int constexpr DEFAULT_SIZE = 30; ///< Default grid size, if not given to the constructor.
-  static float constexpr PIXEL_SIZE = 16.0f; ///< Size of a cell.
+  static constexpr unsigned int DEFAULT_SIZE = 31; ///< Default grid size, if not given to the constructor.
+  static constexpr float PIXEL_SIZE = 16.0f; ///< Size of a cell.
+  static constexpr int MAX_AGE = 6; ///< Used for coloring purposes.
+  static const glm::vec4 CELL_COLOR; ///< Color of alive cells.
 
   /// Initialize an empty grid of given size.
   GameOfLife(unsigned int width, unsigned int height);
@@ -27,6 +29,8 @@ public:
   void mouseMoved(int x, int y) override;
 
 private:
+  /// Changes color to be used to draw a cell.
+  static void setColor(int age, piksel::Graphics& g);
   /// Helper method that converts a pair (row,col) into a grid index.
   int gridIndex(int row, int col) const;
   /// Helper method that "shifts" integers according to modulo arithmetic.
@@ -38,7 +42,7 @@ private:
   // Helper method that "inverts" the value of a grid spot.
   inline void toggle(int row, int col) { toggle(gridIndex(row,col)); };
   // Helper method that "inverts" the value of a grid spot.
-  inline void toggle(int i) { grid_[i] = 1-grid_[i]; };
+  void toggle(int i);
 
   // Count the (alive) neighbours of a given cell.
   int aliveNeighbours(int row, int col);
@@ -51,7 +55,7 @@ private:
   const unsigned int HEIGHT; ///< Height of the grid.
   const unsigned int& ROWS; ///< Number of grid rows. It is an alias of HEIGHT.
   const unsigned int& COLS; ///< Number of grid columns. It is an alias of WIDTH.
-  std::vector<int> grid_; ///< Occupancy grid (`0` means the cell is free).
+  std::vector<int> grid_; ///< Occupancy grid (`0` means the cell is free, other values give the "age" of a cell).
 
   // behavior-related variables
   int update_counter_; ///< When it reaches zero, the grid is updated.
